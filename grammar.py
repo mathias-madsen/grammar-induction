fix sign bug; fix grad accumulation mistake

grammar.py
@@ -27,7 +27,7 @@ class Grammar(torch.nn.Module):
 
         # binary probabilities of the binary choice:
         logemitprobs = -torch.nn.functional.softplus(-self.logit_emitprobs)
-        logbranchprobs = -torch.nn.functional.softplus(-self.logit_emitprobs)
+        logbranchprobs = -torch.nn.functional.softplus(+self.logit_emitprobs)
 
         # conditional probabilities, assuming the binary choice:
         logtnorms = torch.logsumexp(self.logtrans, axis=(1, 2), keepdims=True)
@@ -42,18 +42,9 @@ class Grammar(torch.nn.Module):
 
     def get_trans_and_emits(self) -> Tuple[torch.Tensor, torch.Tensor]:
 
-        # binary probabilities of the binary choice:
-        emitprobs = torch.sigmoid(self.logit_emitprobs)
-
-        # conditional probabilities, assuming the binary choice:
-        logtransflat = self.logtrans.reshape([self.nrules, -1])
-        transflat = torch.softmax(logtransflat, dim=1)
-        cond_trans = transflat.reshape([self.nrules, self.nrules, self.nrules])
-        cond_emits = torch.softmax(self.logemits, dim=1)
-
-        # joint probabilities of the combined choice:
-        trans = cond_trans * (1 - emitprobs[:, None, None])
-        emits = cond_emits * emitprobs[:, None]
+        logtrans, logemits = self.get_logtrans_and_logemits()
+        trans = torch.exp(logtrans)
+        emits = torch.exp(logemits)
 
         transsums = trans.sum(axis=(1, 2))
         emitssums = emits.sum(axis=1)

learn_palindromes.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 from typing import Sequence, Tuple, Optional, Dict, List
 
 from grammar import Grammar
-from parsing import compile_likelihood_table
+from parsing import compile_log_likes
 
 
 def sample_palindrome(alphabet_size: int,
@@ -57,12 +57,12 @@ if __name__ == "__main__":
             # print(len(indices), end=", ", flush=True)
     
             trans, emits = model.get_trans_and_emits()
-            likelihoods = compile_likelihood_table(trans, emits, indices)
-            fullspanlikes = likelihoods[0, len(indices)]
-            loss = -torch.log(fullspanlikes[0])
+            loglikelihoods = compile_log_likes(trans, emits, indices)
+            fullspanloglikes = loglikelihoods[0, len(indices)]
+            loss = -fullspanloglikes[0]  # neg log like under nonterm S
             loss.backward()
     
-            total_loss += loss
+            total_loss += float(loss)
             total_sents += 1
             total_chars += len(indices)
     